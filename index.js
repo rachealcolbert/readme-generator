62f639ab07f31717416496c556b804427cf4df8e
Racheal Colbert <rachealcolbert16@gmail.com>
2020-11-21 13:49:40 -0700

    added validations to questions

--- index.js
+++ index.js
@@ -9,22 +9,54 @@ const questions = () => {
                 type: 'input',
                 name: 'gitHubUsername',
                 message: 'What is your GitHub username?',
+                validate: nameInput => {
+                    if (nameInput) {
+                        return true;
+                    } else {
+                        console.log('Please enter your GitHub username.');
+                    }
+                    return false;
+                }
             },
             {
                 type: 'input',
                 name: 'emailAddress',
                 message: 'What is your email address?',
+                validate: nameInput => {
+                    if (nameInput) {
+                        return true;
+                    } else {
+                        console.log('Please enter your email address.');
+                    }
+                    return false;
+                }
             },
 
             {
                 type: 'input',
                 name: 'titleOfProject',
                 message: 'What is the title of your project?',
+                validate: nameInput => {
+                    if (nameInput) {
+                        return true;
+                    } else {
+                        console.log('Please enter the title of your project.');
+                    }
+                    return false;
+                }
             },
             {
                 type: 'input',
                 name: 'projectDescription',
                 message: 'Please write a short description of your project',
+                validate: nameInput => {
+                    if (nameInput) {
+                        return true;
+                    } else {
+                        console.log('Please enter a description of your project.');
+                    }
+                    return false;
+                }
             },
             {
                 type: 'checkbox',
@@ -51,21 +83,53 @@ const questions = () => {
                 type: 'input',
                 name: 'commandDependencies',
                 message: 'What command should be run to install dependencies?',
+                validate: nameInput => {
+                    if (nameInput) {
+                        return true;
+                    } else {
+                        console.log('Please enter a command.');
+                    }
+                    return false;
+                }
             },
             {
                 type: 'input',
                 name: 'commandTests',
                 message: 'What command should be run to run tests?',
+                validate: nameInput => {
+                    if (nameInput) {
+                        return true;
+                    } else {
+                        console.log('Please enter a command.');
+                    }
+                    return false;
+                }
             },
             {
                 type: 'input',
                 name: 'repoInfo',
                 message: 'What does the user need to know about using the repo?',
+                validate: nameInput => {
+                    if (nameInput) {
+                        return true;
+                    } else {
+                        console.log('Please enter a message to the user.');
+                    }
+                    return false;
+                }
             },
             {
                 type: 'input',
                 name: 'contributingInfo',
-                message: 'What does the user need to know about contributing to the repo?'
+                message: 'What does the user need to know about contributing to the repo?',
+                validate: nameInput => {
+                    if (nameInput) {
+                        return true;
+                    } else {
+                        console.log('Please enter instructions for the user.');
+                    }
+                    return false;
+                }
             },
         ])
 };
