980a3dac41018897d567fd3c2aec6c9b53a6f640
Racheal Colbert <rachealcolbert16@gmail.com>
2020-11-21 12:33:02 -0700

    question prompt feature

--- index.js
+++ index.js
@@ -1,77 +1,77 @@
+const inquirer = require('inquirer');
+
 console.log('Hello! Create a README for your project by answering the following questions');
 
 // array of questions for user
-const questions = [{
-        type: 'input',
-        name: 'gitHubUsername',
-        message: 'What is your GitHub username?',
-    },
-    {
-        type: 'input',
-        name: 'emailAddress',
-        message: 'What is your email address?',
-    },
+inquirer
+    .prompt([{
+            type: 'input',
+            name: 'gitHubUsername',
+            message: 'What is your GitHub username?',
+        },
+        {
+            type: 'input',
+            name: 'emailAddress',
+            message: 'What is your email address?',
+        },
 
-    {
-        type: 'input',
-        name: 'titleOfProject',
-        message: 'What is the title of your project?',
-    },
-    {
-        type: 'input',
-        name: 'projectDescription',
-        message: 'Please write a short description of your project',
-    },
-    {
-        type: 'checkbox',
-        name: 'licenses',
-        message: 'What kind of license should your project have?',
-        choices: [{
-                name: 'MIT',
-            },
-            {
-                name: 'APACHE 2.0',
-            },
-            {
-                name: 'GPL 3.0',
-            },
-            {
-                name: 'BSD 3',
-            },
-            {
-                name: 'None',
-            },
-        ]
-    },
-    {
-        type: 'input',
-        name: 'commandDependencies',
-        message: 'What command should be run to install dependencies?',
-    },
-    {
-        type: 'input',
-        name: 'commandTests',
-        message: 'What command should be run to run tests?',
-    },
-    {
-        type: 'input',
-        name: 'repoInfo',
-        message: 'What does the user need to know about using the repo?',
-    },
-    {
-        type: 'input',
-        name: 'contributingInfo',
-        message: 'What does the user need to know about contributing to the repo?'
-    },
-];
-
+        {
+            type: 'input',
+            name: 'titleOfProject',
+            message: 'What is the title of your project?',
+        },
+        {
+            type: 'input',
+            name: 'projectDescription',
+            message: 'Please write a short description of your project',
+        },
+        {
+            type: 'checkbox',
+            name: 'licenses',
+            message: 'What kind of license should your project have?',
+            choices: [{
+                    name: 'MIT',
+                },
+                {
+                    name: 'APACHE 2.0',
+                },
+                {
+                    name: 'GPL 3.0',
+                },
+                {
+                    name: 'BSD 3',
+                },
+                {
+                    name: 'None',
+                },
+            ]
+        },
+        {
+            type: 'input',
+            name: 'commandDependencies',
+            message: 'What command should be run to install dependencies?',
+        },
+        {
+            type: 'input',
+            name: 'commandTests',
+            message: 'What command should be run to run tests?',
+        },
+        {
+            type: 'input',
+            name: 'repoInfo',
+            message: 'What does the user need to know about using the repo?',
+        },
+        {
+            type: 'input',
+            name: 'contributingInfo',
+            message: 'What does the user need to know about contributing to the repo?'
+        },
+    ])
+    .then(answers => console.log(answers));
 // function to write README file
-function writeToFile(fileName, data) {}
+// function writeToFile(fileName, data) {}
 
-// function to initialize program
-function init() {
+// // function to initialize program
 
-}
-
-// function call to initialize program
-init();
+// // function call to initialize program
+// init();
