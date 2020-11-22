f014cbb21873810143cb0ddb0c00d32ee8ded2fb
Racheal Colbert <rachealcolbert16@gmail.com>
2020-11-21 17:16:23 -0700

    prompt questions working

--- index.js
+++ index.js
@@ -5,130 +5,136 @@ const generateMarkdown = require('./Develop/utils/generateMarkdown')
 console.log('Hello! Create a README for your project by answering the following questions');
 
 // array of questions for user
-const questions = [{
-        type: 'input',
-        name: 'gitHubUsername',
-        message: 'What is your GitHub username?',
-        validate: nameInput => {
-            if (nameInput) {
-                return true;
-            } else {
-                console.log('Please enter your GitHub username.');
+const questions = () => {
+    return inquirer.prompt([{
+            type: 'input',
+            name: 'gitHubUsername',
+            message: 'What is your GitHub username?',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log('Please enter your GitHub username.');
+                }
+                return false;
             }
-            return false;
-        }
-    },
-    {
-        type: 'input',
-        name: 'emailAddress',
-        message: 'What is your email address?',
-        validate: nameInput => {
-            if (nameInput) {
-                return true;
-            } else {
-                console.log('Please enter your email address.');
+        },
+        {
+            type: 'input',
+            name: 'emailAddress',
+            message: 'What is your email address?',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log('Please enter your email address.');
+                }
+                return false;
             }
-            return false;
-        }
-    },
+        },
 
-    {
-        type: 'input',
-        name: 'titleOfProject',
-        message: 'What is the title of your project?',
-        validate: nameInput => {
-            if (nameInput) {
-                return true;
-            } else {
-                console.log('Please enter the title of your project.');
+        {
+            type: 'input',
+            name: 'titleOfProject',
+            message: 'What is the title of your project?',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log('Please enter the title of your project.');
+                }
+                return false;
             }
-            return false;
-        }
-    }, {
-        type: 'input',
-        name: 'projectDescription',
-        message: 'Please write a short description of your project',
-        validate: nameInput => {
-            if (nameInput) {
-                return true;
-            } else {
-                console.log('Please enter a description of your project.');
+        }, {
+            type: 'input',
+            name: 'projectDescription',
+            message: 'Please write a short description of your project',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log('Please enter a description of your project.');
+                }
+                return false;
             }
-            return false;
-        }
-    }, {
-        type: 'checkbox',
-        name: 'licenses',
-        message: 'What kind of license should your project have?',
-        choices: [{
-                name: 'MIT',
-            },
-            {
-                name: 'APACHE 2.0',
-            },
-            {
-                name: 'GPL 3.0',
-            },
-            {
-                name: 'BSD 3',
-            },
-            {
-                name: 'None',
-            },
-        ]
-    }, {
-        type: 'input',
-        name: 'commandDependencies',
-        message: 'What command should be run to install dependencies?',
-        validate: nameInput => {
-            if (nameInput) {
-                return true;
-            } else {
-                console.log('Please enter a command.');
+        }, {
+            type: 'checkbox',
+            name: 'licenses',
+            message: 'What kind of license should your project have?',
+            choices: [{
+                    name: 'MIT',
+                },
+                {
+                    name: 'APACHE 2.0',
+                },
+                {
+                    name: 'GPL 3.0',
+                },
+                {
+                    name: 'BSD 3',
+                },
+                {
+                    name: 'None',
+                },
+            ]
+        }, {
+            type: 'input',
+            name: 'commandDependencies',
+            message: 'What command should be run to install dependencies?',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log('Please enter a command.');
+                }
+                return false;
             }
-            return false;
-        }
-    }, {
-        type: 'input',
-        name: 'commandTests',
-        message: 'What command should be run to run tests?',
-        validate: nameInput => {
-            if (nameInput) {
-                return true;
-            } else {
-                console.log('Please enter a command.');
+        }, {
+            type: 'input',
+            name: 'commandTests',
+            message: 'What command should be run to run tests?',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log('Please enter a command.');
+                }
+                return false;
             }
-            return false;
-        }
-    }, {
-        type: 'input',
-        name: 'repoInfo',
-        message: 'What does the user need to know about using the repo?',
-        validate: nameInput => {
-            if (nameInput) {
-                return true;
-            } else {
-                console.log('Please enter a message to the user.');
+        }, {
+            type: 'input',
+            name: 'repoInfo',
+            message: 'What does the user need to know about using the repo?',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log('Please enter a message to the user.');
+                }
+                return false;
             }
-            return false;
-        }
-    }, {
-        type: 'input',
-        name: 'contributingInfo',
-        message: 'What does the user need to know about contributing to the repo?',
-        validate: nameInput => {
-            if (nameInput) {
-                return true;
-            } else {
-                console.log('Please enter instructions for the user.');
+        }, {
+            type: 'input',
+            name: 'contributingInfo',
+            message: 'What does the user need to know about contributing to the repo?',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log('Please enter instructions for the user.');
+                }
+                return false;
             }
-            return false;
-        }
-    },
-]
+        },
+
+    ])
+};
+
+questions().then(answers => console.log(answers));
+
 // function to write README file
 function writeToFile(fileName, data) {
-    fs.writeFile('README.md', generateMarkdown(answers), err => {
+    fs.writeFile('README.md', generateMarkdown(data), err => {
         if (err) throw err;
         console.log('Hell yeah!')
     });
