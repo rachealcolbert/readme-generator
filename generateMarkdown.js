0b0cc8305f9d1bcc4b462798bcc6c3d32e7bfaf0
Racheal Colbert <rachealcolbert16@gmail.com>
2020-11-22 16:02:42 -0700

    readme template created in generate markdown

--- generateMarkdown.js
+++ generateMarkdown.js
@@ -5,6 +5,24 @@ function generateMarkdown(data) {
 ${data.projectDescription}
 ## License
 ![License badge](https://img.shields.io/badge/license-${data.licenses}-green)
+## Table of Contents
+* [Installation](#installation)
+* [Usage](#usage)
+* [Credits](#credits)
+* [License](#license)
+## Installation
+The command that should be run to install dependencies is${commandDependencies}.
+## Usage 
+${repoInfo}
+Make sure to include a picture of your project. 
+## Credits
+The creator of this project's email and GitHub is linked below. 
+${emailAddress}
+${gitHubUsername}
+## Contributing
+${contributingInfo}
+## Tests
+Use the ${commandTests} command to run a test.
 `;
 }
 
